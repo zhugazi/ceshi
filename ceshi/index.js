84481cc0e7d6461a5ef3681431f2b2802fd08e9b
zhugazi <zhu_gazi@163.com>
2017-11-18 16:56:39 +0800

    ceshi

---
+++ ceshi/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dell on 2017/11/18.
+ */
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+server.listen(8888);
+console.log('Server running at http://127.0.0.1:8888/');
